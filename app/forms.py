ae191d882f16ad0e71b0bb4d46c8a096c2d93a3b
HassanahAl-Saba <hassanah.al-saba@hotmail.com>
2021-04-19 23:08:18 -0500

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,12 @@
+from flask_uploads import UploadSet, IMAGES
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 
+images = UploadSet('images', IMAGES)
+
+class UploadForm(FlaskForm):
+    photo = FileField('image', validators=[
+        FileRequired(),
+        FileAllowed(images, 'Images only!')
+    ])
+    description = StringField('Description', validators = [InputRequired()])
